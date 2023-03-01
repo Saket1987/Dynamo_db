63a2838a6f9c0d0b2ac583d3469387858da37643
Saket1987 <121894053+Saket1987@users.noreply.github.com>
2023-03-02 01:07:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+// In this file, we create the DynamoDB table using the aws_dynamodb_table resource block. We reference our table_name variable to set the name of the table. We also define an attribute with the name specified in our attribute_name variable and type specified in our local.attribute_type variable.
+//We then define a key schema with the attribute_name as the partition key and set the key_type to "HASH". Finally, we set the billing mode to "PAY_PER_REQUEST".
+
 resource "aws_dynamodb_table" "example_table" {
   name = var.table_name
 
