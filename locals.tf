a383ed2b82102a8a06c75e928322da1b9e2a9eac
Saket1987 <121894053+Saket1987@users.noreply.github.com>
2023-03-02 01:04:19 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,3 +1,5 @@
+// In this file, we define a local variable called attribute_type and set it to "S". 
+//This will be used in our main.tf file to specify the type of the attribute_name attribute.
 locals {
   attribute_type = "S"
 }
